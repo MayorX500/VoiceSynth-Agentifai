1ee29ca763c640f45ac59317a544d0be9d076a8d
MayorX500 <g0mes.miguel.0120@gmail.com>
2024-11-24 20:59:36 +0000

    chore: add flag to arguments

--- app_client/__main__.py
+++ app_client/__main__.py
@@ -9,6 +9,6 @@ def main(args):
 if __name__ == "__main__":
     parser = ap.ArgumentParser()
     parser.add_argument("ipadd", type=str, help="Ip address of the server")
-    parser.add_argument("--debug","-d", action="store_true", help="Enable debug mode", default=False)
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode", default=False)
     args = parser.parse_args()
     main(args)
