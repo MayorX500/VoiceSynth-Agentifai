c3413abfc896cb061305ca5a543f6b8db7ff291c
MayorX500 <g0mes.miguel.0120@gmail.com>
2024-11-24 20:52:47 +0000

    chore: move grpcs to correct location

--- grpcs/generate_protos.sh
+++ grpcs/generate_protos.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3.10 -m grpc_tools.protoc -I=proto --python_out=server --grpc_python_out=server proto/tts.proto
-python3.10 -m grpc_tools.protoc -I=proto --python_out=client --grpc_python_out=client proto/tts.proto
+python3.10 -m grpc_tools.protoc -I=proto --python_out=app_server --grpc_python_out=app_server proto/tts.proto
+python3.10 -m grpc_tools.protoc -I=proto --python_out=app_client --grpc_python_out=app_client proto/tts.proto
