bc1a04db804a20e86af4cf639f9af99c1aa01eb7
MayorX500 <g0mes.miguel.0120@gmail.com>
2024-12-03 15:32:20 +0000

    fix:use latest model

--- scripts/download_model.sh
+++ scripts/download_model.sh
@@ -29,8 +29,8 @@ download_files() {
 	## Download files required for the model
 	download_file "http://mayorx.xyz/Media/agentifai/input_voice.wav" "inputs/voices/input_voice.wav"
 	download_file "https://huggingface.co/coqui/XTTS-v2/resolve/main/vocab.json" "model/run/training/XTTS_v2.0_original_model_files/vocab.json"
-	download_file "http://mayorx.xyz/Media/agentifai/model/config.json" "model/run/training/GPT_XTTS_Finetuned/config.json"
-	download_file "http://mayorx.xyz/Media/agentifai/model/best_model.pth" "model/run/training/GPT_XTTS_Finetuned/best_model.pth"
+	download_file "http://mayorx.xyz/Media/agentifai/config.json" "model/run/training/GPT_XTTS_Finetuned/config.json"
+	download_file "http://mayorx.xyz/Media/agentifai/last_gen.pth" "model/run/training/GPT_XTTS_Finetuned/best_model.pth"
 }
 
 main() {
@@ -45,4 +45,3 @@ main() {
 }
 
 main
-
